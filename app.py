eaf7c9c76c6ec0eec379341ecc63e0ec13b14154
Christian Fosli <cfosli@gmail.com>
2019-05-28 21:41:04 +0200

    Add /highscores page, refactor templates

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app.secret_key = b'\x13\xee\x87\xf7E\x01\xc9\xac5\xaf\x1c\xe2w\xb8(\xc1\xea^\xd9
 def index():
     return render_template('index.html')
 
+@app.route('/highscores')
+def highscores():
+    return render_template('highscores.html')
+
 @app.route('/score', methods=['GET', 'POST'])
 def score():
     max_score = session.get('max_score', 0)
