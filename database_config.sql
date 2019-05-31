a7ff65e176e8cabb1979d601c42c595a6b0e4c09
Christian Fosli <cfosli@gmail.com>
2019-05-31 19:23:34 +0200

    Add stored procedure for clean up database
    Manually applied to databases

--- database_config.sql
+++ database_config.sql
@@ -15,6 +15,17 @@ create or replace view top_ten_month as select * from highscore H
 where month(H.sdate) = month(CURRENT_DATE())
 order by H.score desc, H.sdate asc limit 10;
 
+delimiter //
+create procedure cleanup_highscores()
+    begin
+    delete from highscore
+    where (username, score, sdate) not in
+        (select * from top_ten_month)
+        and (username, score, sdate) not in
+        (select * from top_ten_all);
+    end //
+delimiter ;
+
 /* If dev */
 create user 'visnake-admin'@'localhost' identified by 'secret123';
 grant all privileges on visnake.* to 'visnake-admin'@'localhost';
